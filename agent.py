d074c822a6c87b1a053ca50478d69eaff307fea1
kewei <kewei.train@gmail.com>
2019-09-30 01:25:25 +0800

    update

--- agent.py
+++ agent.py
@@ -1,4 +1,3 @@
-import collections
 import random
 
 JUMP = 0
@@ -12,20 +11,20 @@ class EpsilonGreedy:
 class Agent:
     def __init__(self, policy):
         # (dx, dy) -> [award of jump, award of do nothing]
-        self.__Qtable = {}
-        self.__award = 1
-        self.__penalty = -100
-        self.__policy = policy
-        self.__prevAction = PAUSE
+        self.qtable = {}
+        self.award = 1
+        self.penalty = -100
+        self.policy = policy
+        self.prevAction = PAUSE
 
     # jump returns true if decide to jump this moment
     def jump(self, player, upipe, lpipe):
         dx = upipe['x'] - player['x']
         dy = (lpipe['y'] + upipe['y'])/2.0 - player['y']
-        if (dx, dy) not in self.__Qtable:
-            self.__Qtable[(dx, dy)] = [1.0, 1.0]
-        action = self.__policy.takeAction(self.__Qtable[(dx, dy)])
-        self.__prevAction = action
+        if (dx, dy) not in self.qtable:
+            self.qtable[(dx, dy)] = [1.0, 1.0]
+        action = self.policy.takeAction(self.qtable[(dx, dy)])
+        self.prevAction = action
         return action == JUMP
 
     # feeback gives feedback of previous action
